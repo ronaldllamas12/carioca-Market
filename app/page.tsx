95f5d481aaca2a28dfad9c99cc58c70cf3a01888
Ronald <ronaldllamas17@gmail.com>
2025-06-24 01:07:13 -0500

    Actualizar page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion';
 export default function Home() {
   return (
     <main
-      className="flex flex-col items-center justify-center min-h-screen px-6 md:px-10 pb-12 max-w-screen-xl mx-auto bg-black relative overflow-hidden"
+      className="flex flex-col items-center justify-center min-h-screen-m px-6 md:px-10 pb-12 max-w-screen-xl mx-auto bg-black relative overflow-hidden"
       style={{
         backgroundImage: "url('/Bienvenido al Marketplace Iguazú.png')",
         backgroundSize: 'contain',
